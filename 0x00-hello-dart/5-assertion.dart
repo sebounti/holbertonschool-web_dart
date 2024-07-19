c8999355cb0adb75a3e330f86129f36c015a109f
lenne sebastien <sebastienlenne@gmail.com>
2024-07-19 12:58:50 +0200

    add all task for project hello dart

---
+++ 0x00-hello-dart/5-assertion.dart
@@ -0,0 +1,7 @@
+void main(List<String> args) {
+  int score = int.parse(args[0]);
+
+  assert(score >= 80,
+      'Uncaught Error: Assertion failed: "The score must be bigger or equal to 80');
+  print('You Passed');
+}
