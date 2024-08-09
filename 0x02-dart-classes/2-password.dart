79749bd0762f24871360aeec00a4a1cb5a051a52
lenne sebastien <sebastienlenne@gmail.com>
2024-08-09 11:18:11 +0200

    [update]  task 2 from project dart-classes

--- 0x02-dart-classes/2-password.dart
+++ 0x02-dart-classes/2-password.dart
@@ -5,26 +5,26 @@ class Password {
 // CONSTRUCTEUR
   Password() : password = '';
 
-  String isValid() {
+  bool isValid() {
     // verify if the password length is between 8 and 16 characters
     if (password.length < 8 || password.length > 16) {
-      return 'Password length must be between 8 and 16 characters';
+      return false;
     }
     // verify if the password contains at least one uppercase letter
     if (password.contains(RegExp(r'[A-Z]')) == false) {
-      return 'Password must contain at least one uppercase letter';
+      return false;
     }
     // verify if the password contains at least one lowercase letter
     if (password.contains(RegExp(r'[a-z]')) == false) {
-      return 'Password must contain at least one lowercase letter';
+      return false;
     }
     // verify if the password contains at least one digit
     if (password.contains(RegExp(r'[0-9]')) == false) {
-      return 'Password must contain at least one digit';
+      return false;
     }
     ;
 
-    return 'Password is valid';
+    return true;
   }
 
   String toString() {
