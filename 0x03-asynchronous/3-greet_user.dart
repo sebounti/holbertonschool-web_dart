f4e209c7b5e3246832c3f8499b85d4ba57dbf7c7
lenne sebastien <sebastienlenne@gmail.com>
2024-08-11 17:24:05 +0100

    [add] new task 3 for project dart - asynchronous

---
+++ 0x03-asynchronous/3-greet_user.dart
@@ -0,0 +1,29 @@
+import 'dart:async';
+import 'dart:convert';
+import '3-util.dart';
+
+Future<String> greetUser() async {
+  try {
+    String UserData = await fetchUserData();
+    Map<String, dynamic> userMap = jsonDecode(UserData);
+    String username = userMap['username'];
+    return 'Hello, $username';
+  } catch (e) {
+    return 'error caught: $e';
+  }
+}
+
+Future<String> loginUser() async {
+  try {
+    bool credentialsValid = await checkCredentials();
+    if (credentialsValid) {
+      print('There is a user: true');
+      return (await greetUser());
+    } else {
+      print('There is a user: false');
+      return 'Wongs credentials';
+    }
+  } catch (e) {
+    return 'error caught: $e';
+  }
+}
