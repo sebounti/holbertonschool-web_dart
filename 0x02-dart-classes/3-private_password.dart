a3efa7c9072c385b02b6369d78c78f9294cb0b00
lenne sebastien <sebastienlenne@gmail.com>
2024-11-04 14:26:08 +0100

    add task 3 for project dart class

---
+++ 0x02-dart-classes/3-private_password.dart
@@ -0,0 +1,34 @@
+class Password {
+  // PROPRIETE
+  String _password;
+
+  // CONSTRUCTEUR
+  Password({required String password}) : _password = password {
+    // Add an empty function body for the constructor
+  }
+
+  bool isValid() {
+    // verify if the password length is between 8 and 16 characters
+    if (_password.length < 8 || _password.length > 16) {
+      return false;
+    }
+    // verify if the password contains at least one uppercase letter
+    if (_password.contains(RegExp(r'[A-Z]')) == false) {
+      return false;
+    }
+    // verify if the password contains at least one lowercase letter
+    if (_password.contains(RegExp(r'[a-z]')) == false) {
+      return false;
+    }
+    // verify if the password contains at least one digit
+    if (_password.contains(RegExp(r'[0-9]')) == false) {
+      return false;
+    }
+    ;
+    return true;
+  }
+
+  String toString() {
+    return 'Your Password is: $_password';
+  }
+}
