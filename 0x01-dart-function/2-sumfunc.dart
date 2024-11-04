e2428750e8ed2818fc6fddeefccc30b82669cc57
lenne sebastien <sebastienlenne@gmail.com>
2024-11-04 10:42:09 +0100

    add task 2

---
+++ 0x01-dart-function/2-sumfunc.dart
@@ -0,0 +1,13 @@
+int add(int a, int b) {
+  return a + b;
+}
+
+int sub(int a, int b) {
+  return a - b;
+}
+
+String showFunc(int a, int b) {
+  int addition = add(a, b);
+  int subtraction = sub(a, b);
+  return 'Add $a + $b = $addition\nSub $a - $b = $subtraction';
+}
