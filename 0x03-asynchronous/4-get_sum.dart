305a7d3963e70cd8c465ddd68b828811f175b023
lenne sebastien <sebastienlenne@gmail.com>
2024-11-05 09:26:57 +0100

    update  task 4 for project dart asynchronous

--- 0x03-asynchronous/4-get_sum.dart
+++ 0x03-asynchronous/4-get_sum.dart
@@ -1,26 +1,27 @@
-import 'dart:convert';
-import '4-util.dart';
+import "4-util.dart";
+import "dart:convert";
 
 Future<double> calculateTotal() async {
   try {
-    String userData = await fetchUserData();
-    Map<String, dynamic> userMap = jsonDecode(userData);
-    String userId = userMap['id'];
-
-    String ordersData = await fetchUserOrders(userId);
-    List<dynamic> orders = jsonDecode(ordersData);
-
-    double totalPrice = 0.0;
-
-    for (String product in orders) {
-      String priceData = await fetchProductPrice(product);
-      double price = double.parse(priceData);
-      totalPrice += price;
-    }
-
-    return totalPrice;
+    // Call fetchUserData
+    return fetchUserData().then((user) {
+      // Convert string response to json
+      String id = json.decode(user)["id"];
+      // Asynchronously call fetchUserOrders with id of user from fetchUserData
+      return fetchUserOrders(id).then((products) async {
+        double totalPrice = 0.0;
+        // Convert string response to json and loop through product list
+        for (String product in json.decode(products)) {
+          // As each product is fetched, call fetchProductPrice with product
+          await fetchProductPrice(product).then((price) {
+            // Convert string response to json and add price to total
+            totalPrice += json.decode(price);
+          });
+        }
+        return totalPrice;
+      });
+    });
   } catch (e) {
-    print('error caught: $e');
-    return (-1);
+    return -1;
   }
 }
