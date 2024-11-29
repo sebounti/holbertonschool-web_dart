a7aeb8497f4c064cf69b3c8b98b2315ff18929ed
lenne sebastien <sebastienlenne@gmail.com>
2024-11-29 11:42:04 +0100

    add update file

--- 0x03-asynchronous/rick_and_morty.dart
+++ 0x03-asynchronous/rick_and_morty.dart
@@ -1,31 +1,22 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 
-/// Fetches and prints all character names from the Rick and Morty API.
-///
-/// This function:
-/// 1. Makes a GET request to the Rick and Morty API.
-/// 2. Checks if the response was successful (200).
-/// 3. If the response was successful, it decodes the JSON response and
-///    prints all character names.
-/// 4. If the response was not successful, it prints an error message.
-/// 5. If an exception occurs, it prints the error message.
 Future<void> printRmCharacters() async {
-  const String apiUrl = 'https://rickandmortyapi.com/api/character';
-
   try {
-    final response = await http.get(Uri.parse(apiUrl));
+    final url = Uri.parse('https://rickandmortyapi.com/api/character');
+    final response = await http.get(url);
+
     if (response.statusCode == 200) {
       final data = jsonDecode(response.body);
-      final List characters = data['results'];
 
+      final characters = data['results'] as List;
       for (var character in characters) {
         print(character['name']);
       }
     } else {
-      print('Error: Failed to fetch characters.');
+      throw 'Failed to load data with status code: ${response.statusCode}';
     }
-  } catch (e) {
-    print('error caught: $e');
+  } catch (error) {
+    print('error caught: $error');
   }
 }
