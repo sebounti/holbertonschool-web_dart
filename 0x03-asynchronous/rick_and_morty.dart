1e0a46f8a34e81b809115b146904f2e4c18c3549
lenne sebastien <sebastienlenne@gmail.com>
2024-11-29 11:28:12 +0100

    add update file

--- 0x03-asynchronous/rick_and_morty.dart
+++ 0x03-asynchronous/rick_and_morty.dart
@@ -2,24 +2,21 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 Future<void> printRmCharacters() async {
-  const String apiUrl =
-      'https://rickandmortyapi.com/api/character'; // Rick and Morty API
+  const String apiUrl = 'https://rickandmortyapi.com/api/character';
 
   try {
     final response = await http.get(Uri.parse(apiUrl));
-
     if (response.statusCode == 200) {
-      final data = json.decode(response.body);
-
+      final data = jsonDecode(response.body);
       final List characters = data['results'];
 
       for (var character in characters) {
         print(character['name']);
       }
     } else {
-      print('Error: Failed to load characters');
+      print('Error: Failed to fetch characters.');
     }
   } catch (e) {
-    print('Error caught: $e');
+    print('error caught: $e');
   }
 }
